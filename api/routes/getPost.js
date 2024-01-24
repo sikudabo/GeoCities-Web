8d26163704e4f9cac41e56fadb2fa5e09551a007
Simeon Ikudabo <simeonikudabo@Simeons-Air.lan>
2024-01-24 00:05:04 -0500

    Returning new attributes when a post is returned.

--- api/routes/getPost.js
+++ api/routes/getPost.js
@@ -9,11 +9,11 @@ router.route('/api/get-post/:_id').get(async (req, res) => {
         const post = await PostModel.findOne({ _id });
 
         if (!post) {
-            res.status(200).json({ isError: false, isPostDeleted: true, message: 'This post has been deleted' });
+            res.status(200).json({ isError: false, isPostDeleted: true, message: 'This post has been deleted', post });
             return;
         }
 
-        res.status(200).json({ isError: false, post });
+        res.status(200).json({ isError: false, isPostDeleted: false, post });
         return;
     } catch(err) {
         console.log(`There was an error retrieving post: ${_id}: ${err.message}`);
