c5bfce3840fe5d5bb7ff97f767a220bcbba813c7
Simeon Ikudabo <simeonikudabo@Simeons-Air.lan>
2024-01-24 23:05:56 -0500

    Changing the name of communities blocked from to 'groupsBlockedFrom'

--- db/schemas/userSchema.js
+++ db/schemas/userSchema.js
@@ -15,7 +15,7 @@ const userSchema = mongoose.Schema({
     followers: [String],
     following: [String],
     blockedList: [String],
-    groupsBlockList: { default: [], type: [String ] },
+    groupsBlockedFrom: { default: [], type: [String ] },
     fbId: { required: true, type: String, unique: true },
     blockedFrom: { default: [], type: [String] },
     groups: { default: [], type: [String] },
