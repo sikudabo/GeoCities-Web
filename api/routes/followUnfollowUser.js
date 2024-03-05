56d1295f860d3fa449834cd4b522acd22713afd6
Simeon Ikudabo <simeonikudabo@Simeons-Air.lan>
2024-03-05 16:59:02 -0500

    Adding endpoint to follow and unfollow a user.

--- api/routes/followUnfollowUser.js
+++ api/routes/followUnfollowUser.js
@@ -3,15 +3,17 @@ const router = express.Router();
 const { UserModel } = require('../../db/models');
 
 router.route('/api/follow-unfollow-user').post(async (req, res) => {
-    const { _id, followerId, isFollow } = req.params; 
+    const { _id, followerId, isFollow } = req.body; 
 
     try {
         if (isFollow) {
             await UserModel.updateOne({ _id }, { $push: { followers: followerId }});
+            await UserModel.updateOne({ _id: followerId }, { $push: { following: _id }});
             return res.status(200).json({ isError: false, message: 'Successfully followed user.' });
         }
 
         await UserModel.updateOne({ _id }, { $pull: { followers: followerId }});
+        await UserModel.updateOne({ _id: followerId }, { $pull: { following: _id }});
         return res.status(200).json({ isError: false, message: 'Successfully unfollowed user.' });
     } catch (err) {
         console.log(`There was an error ${isFollow ? 'following' : 'unfollowing'} a user: ${err.message}`);
